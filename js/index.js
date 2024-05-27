19bffb8662563f597e13f8fb995308ece1037a5f
berem-git <berembek05@gmail.com>
2024-05-27 23:11:38 +0600

    t

--- js/index.js
+++ js/index.js
@@ -16,11 +16,17 @@ const localization = {
     erkeHistory3:
       "На Олимпийских играх 2008 года спонсировал женскую сборную Китая по тяжелой атлетике весом до 48 кг, представленную Чэнь Сеся, завоевавшей первую золотую медаль, а также спонсировал КНДР Кореи, завоевавшую две золотые, одну серебряную и три бронзовые медали.",
     xtepHistory1:
-     "Xtep — китайская компания, начавшая свою деятельность с производства спортивного инвентаря, известная сегодня своей высококачественной спортивной одеждой и обувью. Штаб-квартира расположена в Гонконге. Xtep занимается проектированием, разработкой, производством, продажами, маркетингом и бренд-менеджментом спортивного инвентаря, включая обувь, одежду и аксессуары. Это ведущий производитель спортивной обуви и одежды в Китае с обширной розничной сетью из более чем 7500 магазинов, охватывающих 31 провинцию, автономные районы и муниципалитеты по всей территории КНР и за рубежом. Компания «Sanxing Sporting Goods Co., Ltd.» была основана в 1987 году",
+      "Xtep — китайская компания, начавшая свою деятельность с производства спортивного инвентаря, известная сегодня своей высококачественной спортивной одеждой и обувью. Штаб-квартира расположена в Гонконге. Xtep занимается проектированием, разработкой, производством, продажами, маркетингом и бренд-менеджментом спортивного инвентаря, включая обувь, одежду и аксессуары. Это ведущий производитель спортивной обуви и одежды в Китае с обширной розничной сетью из более чем 7500 магазинов, охватывающих 31 провинцию, автономные районы и муниципалитеты по всей территории КНР и за рубежом. Компания «Sanxing Sporting Goods Co., Ltd.» была основана в 1987 году",
     xtepHistory2:
-     "Бренд Xtep создан в 2001 году\n 3 июня 2008 года состоялась официальная регистрация торговой марки на Гонконгской фондовой бирже. В 2019 году объединилась с несколькими международными брендами путем приобретений или создания совместных предприятий, чтобы стать мультибрендовой и интернациональной группой. . В настоящее время Группа управляет шестью спортивными брендами: Xtep, Xtep Kids, Saucony, Merrell, K · SWISS и Palladium. С момента своего создания в 2001 году основной бренд группы “Xtep” имеет обширную дистрибьюторскую сеть из более чем 7500 розничных магазинов, охватывающих 31 провинцию, автономный район и муниципалитет Китая, а также зарубежные регионы. В настоящее время зарубежные продажи продукции в Азии, Европе и на Среднем Востоке.\n 25.11.2021 года было официальное открытие первого магазина XTEP в Кыргызстане г.Бишкек",
+      "Бренд Xtep создан в 2001 году\n 3 июня 2008 года состоялась официальная регистрация торговой марки на Гонконгской фондовой бирже. В 2019 году объединилась с несколькими международными брендами путем приобретений или создания совместных предприятий, чтобы стать мультибрендовой и интернациональной группой. . В настоящее время Группа управляет шестью спортивными брендами: Xtep, Xtep Kids, Saucony, Merrell, K · SWISS и Palladium. С момента своего создания в 2001 году основной бренд группы “Xtep” имеет обширную дистрибьюторскую сеть из более чем 7500 розничных магазинов, охватывающих 31 провинцию, автономный район и муниципалитет Китая, а также зарубежные регионы. В настоящее время зарубежные продажи продукции в Азии, Европе и на Среднем Востоке.\n 25.11.2021 года было официальное открытие первого магазина XTEP в Кыргызстане г.Бишкек",
     xtepHistory3:
       "С момента своего создания в 2001 году основной бренд группы “Xtep” имеет обширную дистрибьюторскую сеть из более чем 7500 розничных магазинов, охватывающих 31 провинцию, автономный район и муниципалитет Китая, а также зарубежные регионы. В настоящее время зарубежные продажи продукции в Азии, Европе и на Среднем Востоке.\n25.11.2021 года было официальное открытие первого магазина XTEP в Кыргызстане г.Бишкек",
+    adidasHistory1:
+      "С момента своего основания в 1949 году, штаб-квартира adidas Group расположена в Херцогенаурахе, Германия. Благодаря десятилетиям упорной работы и инноваций, adidas превратилась в один из ведущих мировых спортивных брендов. Компания объединяет в себе обширные возможности исследований и разработок, производства, продаж и маркетинга, что позволяет ей оставаться на передовой спортивной индустрии.",
+    adidasHistory2:
+      "Adidas активно внедряет инновации и использует новейшие технологии для создания высококачественной продукции. Особое внимание уделяется устойчивому развитию и экологически чистым материалам, что отражается в различных инициативах компании по защите окружающей среды.",
+    adidasHistory3:
+      "Благодаря своей приверженности качеству и инновациям, adidas завоевала доверие миллионов спортсменов и любителей активного образа жизни по всему миру. Компания продолжает расширять своё присутствие на глобальном рынке, открывая новые магазины и улучшая существующие, чтобы обеспечить доступ к своей продукции каждому, кто стремится к лучшим результатам.",
   },
   en: {
     about: "About Us",
@@ -41,11 +47,17 @@ const localization = {
     erkeHistory3:
       "At the 2008 Olympics sponsored the Chinese women's heavyweight team weightlifting up to 48 kg, presented by Chen Xixia, won the first gold medal, as well as sponsored the DPRK of Korea, which won two gold medals, one silver and three bronze medals.",
     xtepHistory1:
-     "Xtep is a Chinese company that began its activities with the production of sports equipment, today known for its high-quality sportswear and shoes. The headquarters is located in Hong Kong. Xtep is engaged in the design, development, production, sales, marketing and brand management of sports equipment, including footwear, apparel and accessories. It is a leading manufacturer of sports shoes and apparel in China, with an extensive retail network of more than 7,500 stores covering 31 provinces, autonomous regions and municipalities in China and abroad. Sanxing Sporting Goods Co., Ltd. was in 1987",
+      "Xtep is a Chinese company that began its activities with the production of sports equipment, today known for its high-quality sportswear and shoes. The headquarters is located in Hong Kong. Xtep is engaged in the design, development, production, sales, marketing and brand management of sports equipment, including footwear, apparel and accessories. It is a leading manufacturer of sports shoes and apparel in China, with an extensive retail network of more than 7,500 stores covering 31 provinces, autonomous regions and municipalities in China and abroad. Sanxing Sporting Goods Co., Ltd. was in 1987",
     xtepHistory2:
-     "The Xtep brand was created in 2001\nOn June 3, 2008, the trademark was officially registered on the Hong Kong Stock Exchange. In 2019, it merged with several international brands through acquisitions or joint ventures to become a multi-brand and international group. . The Group currently operates six sports brands: Xtep, Xtep Kids, Saucony, Merrell, K SWISS and Palladium. Since its establishment in 2001, the group's core brand “Xtep” has an extensive distribution network of more than 7,500 retail stores, covering 31 provinces, autonomous regions and municipalities in China, as well as overseas regions. Currently, foreign sales of products are in Asia, Europe and the Middle East.\n On November 25, 2021, the first XTEP store was officially opened in Bishkek, Kyrgyzstan",
+      "The Xtep brand was created in 2001\nOn June 3, 2008, the trademark was officially registered on the Hong Kong Stock Exchange. In 2019, it merged with several international brands through acquisitions or joint ventures to become a multi-brand and international group. . The Group currently operates six sports brands: Xtep, Xtep Kids, Saucony, Merrell, K SWISS and Palladium. Since its establishment in 2001, the group's core brand “Xtep” has an extensive distribution network of more than 7,500 retail stores, covering 31 provinces, autonomous regions and municipalities in China, as well as overseas regions. Currently, foreign sales of products are in Asia, Europe and the Middle East.\n On November 25, 2021, the first XTEP store was officially opened in Bishkek, Kyrgyzstan",
     xtepHistory3:
       "Since its establishment in 2001, the group's core brand “Xtep” has an extensive distribution network of more than 7,500 retail stores, covering 31 provinces, autonomous regions and municipalities in China, as well as overseas regions. Currently, overseas product sales are in Asia, Europe and the Middle East.\n November 25, 2021 was the official opening of the first XTEP store in Kyrgyzstan, Bishkek",
+    adidasHistory1:
+      "The adidas Group has been headquartered in Herzogenaurach, Germany since its founding in 1949. Through decades of hard work and innovation, adidas has evolved into one of the world's leading sports brands. The company combines extensive research and development, manufacturing, sales and marketing capabilities to stay at the forefront of the sports industry.",
+    adidasHistory2:
+      "Adidas is committed to innovation and utilizes the latest technologies to create high quality products. Special attention is paid to sustainability and eco-friendly materials, which is reflected in the company's various environmental initiatives.",
+    adidasHistory3:
+      "Through its commitment to quality and innovation, adidas has earned the trust of millions of athletes and active lifestyle enthusiasts around the world. The company continues to expand its presence in the global marketplace, opening new stores and improving existing ones to ensure that its products are available to everyone who strives for better performance.",
   },
 
   kg: {
@@ -67,34 +79,48 @@ const localization = {
     erkeHistory3:
       "2008-жылкы Олимпиадада Кытайдын оор салмактагы аялдар командасы тарабынан каржыланган 48 кг чейин оор атлетика, Чен Сисиа тарабынан сунушталган, биринчи алтын медалды, ошондой эле эки алтын медалга ээ болгон Кореянын КЭДРинин демөөрчүлүгү менен, бир күмүш жана үч коло медаль.",
     xtepHistory1:
-     "Xtep - бул кытай өлкөсүүнүн  компаниясы, спорттук жабдууларды чыгарууда өз ишин баштаган, бүгүнкү күндө өзүнүн жогорку сапаттагы спорттук кийимдери жана бут кийимдери менен белгилүү. Башкы кеңсеси Гонконгдо жайгашкан. Xtep спорттук жабдууларды, анын ичинде бут кийимдерди, кийимдерди жана аксессуарларды долбоорлоо, иштеп чыгуу, өндүрүү, сатуу, маркетинг жана бренд башкаруу менен алектенет. Бул Кытайдагы спорттук бут кийимдердин жана кийимдердин алдыңкы өндүрүүчүсү, 7500дөн ашык дүкөндөрдүн кеңири чекене тармагы менен 31 провинцияны, автономиялуу аймактарды жана муниципалитеттерди бүткүл Кытайда жана чет өлкөлөрдө камтыйт. Sanxing Sporting Goods Co., Ltd. 1987-жылы негизделген;",
+      "Xtep - бул кытай өлкөсүүнүн  компаниясы, спорттук жабдууларды чыгарууда өз ишин баштаган, бүгүнкү күндө өзүнүн жогорку сапаттагы спорттук кийимдери жана бут кийимдери менен белгилүү. Башкы кеңсеси Гонконгдо жайгашкан. Xtep спорттук жабдууларды, анын ичинде бут кийимдерди, кийимдерди жана аксессуарларды долбоорлоо, иштеп чыгуу, өндүрүү, сатуу, маркетинг жана бренд башкаруу менен алектенет. Бул Кытайдагы спорттук бут кийимдердин жана кийимдердин алдыңкы өндүрүүчүсү, 7500дөн ашык дүкөндөрдүн кеңири чекене тармагы менен 31 провинцияны, автономиялуу аймактарды жана муниципалитеттерди бүткүл Кытайда жана чет өлкөлөрдө камтыйт. Sanxing Sporting Goods Co., Ltd. 1987-жылы негизделген;",
     xtepHistory2:
-     "Xtep бренди 2001-жылы түзүлгөн\n2008-жылдын 3-июнунда товардык белги Гонконг фондулук биржасында расмий түрдө катталган. 2019-жылы ал сатып алуулар же биргелешкен ишканалар аркылуу бир нече эл аралык бренддер менен биригип, мульти-бренддүү жана эл аралык топко айланды. . Топ учурда алты спорттук брендди башкарат: Xtep, Xtep Kids, Saucony, Merrell, K SWISS жана Palladium. 2001-жылы түзүлгөндөн бери, топтун негизги бренди “Xtep” Кытайдын 31 провинциясын, автономдуу аймактарын жана муниципалитеттерин, ошондой эле чет өлкөлөрдөгү аймактарды камтыган 7500дөн ашык чекене дүкөндөрдүн кеңири жайылтуу тармагына ээ. Учурда товарлардын тышкы сатыктары Азия, Европа жана Жакынкы Чыгышта.\n 2021-жылдын 25-ноябрында биринчи XTEP дүкөнү расмий түрдө Бишкекте, Кыргызстанда ачылган.",
+      "Xtep бренди 2001-жылы түзүлгөн\n2008-жылдын 3-июнунда товардык белги Гонконг фондулук биржасында расмий түрдө катталган. 2019-жылы ал сатып алуулар же биргелешкен ишканалар аркылуу бир нече эл аралык бренддер менен биригип, мульти-бренддүү жана эл аралык топко айланды. . Топ учурда алты спорттук брендди башкарат: Xtep, Xtep Kids, Saucony, Merrell, K SWISS жана Palladium. 2001-жылы түзүлгөндөн бери, топтун негизги бренди “Xtep” Кытайдын 31 провинциясын, автономдуу аймактарын жана муниципалитеттерин, ошондой эле чет өлкөлөрдөгү аймактарды камтыган 7500дөн ашык чекене дүкөндөрдүн кеңири жайылтуу тармагына ээ. Учурда товарлардын тышкы сатыктары Азия, Европа жана Жакынкы Чыгышта.\n 2021-жылдын 25-ноябрында биринчи XTEP дүкөнү расмий түрдө Бишкекте, Кыргызстанда ачылган.",
     xtepHistory3:
       "2001-жылы түзүлгөндөн бери, топтун негизги бренди “Xtep” Кытайдын 31 провинциясын, автономдуу аймактарын жана муниципалитеттерин, ошондой эле чет өлкөлөрдөгү аймактарды камтыган 7500дөн ашык чекене дүкөндөрдүн кеңири жайылтуу тармагына ээ. Азыркы учурда, чет продуктыларды сатуу Азияда, Европада жана Жакынкы Чыгышта. 2021-жылдын 25-ноябрында Кыргызстандагы биринчи XTEP дүкөнүнүн расмий ачылышы болду, Бишкек шаарында.",
+    adidasHistory1:
+      "Adidas компаниясы 1949-жылы негизделгенден бери Германиянын Герцогенаурах шаарында жайгашкан. Көп жылдык мээнет жана инновациялар аркылуу adidas дүйнөнүн алдыңкы спорт брендеринин бирине айланды. Компания спорт индустриясында алдыңкы орунда калуу үчүн кеңири изилдөө жана өнүктүрүү, өндүрүш, сатуу жана маркетинг мүмкүнчүлүктөрүн бириктирет.",
+
+    adidasHistory2:
+      "Adidas инновацияларга берилген жана эң акыркы технологияларды колдонуу менен жогорку сапаттагы продукцияларды түзөт. Туруктуу өнүгүү жана экологиялык таза материалдарга өзгөчө көңүл бурулат, бул компаниянын ар кандай экологиялык демилгелеринде чагылдырылат.",
+
+    adidasHistory3:
+      "Сапатка жана инновацияларга берилгендиги аркылуу adidas дүйнө жүзүндөгү миллиондогон спортчулардын жана активдүү жашоо образына ынтызар адамдардын ишенимине ээ болду. Компания глобалдык рынокто өзүнүн катышуусун кеңейтүүнү улантууда, жаңы дүкөндөрдү ачып жана бар болгон дүкөндөрдү жакшыртып, анын продукциялары жакшыраак көрсөткүчтөргө умтулган ар бир адамга жеткиликтүү болушун камсыздайт.",
   },
 };
 
 function changeLanguage(language) {
+  document.getElementById("adidasHistory1").textContent =
+  localization[language].adidasHistory1;
+document.getElementById("adidasHistory2").textContent =
+  localization[language].adidasHistory2;
+document.getElementById("adidasHistory3").textContent =
+  localization[language].adidasHistory3;
+
   document.getElementById("erkeHistory1").textContent =
     localization[language].erkeHistory1;
   document.getElementById("erkeHistory2").textContent =
     localization[language].erkeHistory2;
   document.getElementById("erkeHistory3").textContent =
     localization[language].erkeHistory3;
+
   document.getElementById("xtepHistory1").textContent =
     localization[language].xtepHistory1;
   document.getElementById("xtepHistory2").textContent =
     localization[language].xtepHistory2;
   document.getElementById("xtepHistory3").textContent =
     localization[language].xtepHistory3;
+
   document.getElementById("royalBrends").textContent =
     localization[language].royalBrends;
-    document.getElementById("royalBrendsDescription").textContent =
+  document.getElementById("royalBrendsDescription").textContent =
     localization[language].royalBrendsDescription;
-  document.getElementById("welcom").textContent =
-    localization[language].welcom;
-
 
   document.getElementById("about").textContent = localization[language].about;
   document.getElementById("brand").textContent = localization[language].brand;
@@ -103,12 +129,14 @@ function changeLanguage(language) {
   document.getElementById("geography").textContent =
     localization[language].geography;
 
+
+
   localStorage.setItem("preferredLanguage", language);
-  document.querySelectorAll('.header-language span').forEach(span => {
-    span.classList.remove('selected');
-});
-document.getElementById(`lang-${language}`).classList.add('selected');
-localStorage.setItem("preferredLanguage", language);
+  document.querySelectorAll(".header-language span").forEach((span) => {
+    span.classList.remove("selected");
+  });
+  document.getElementById(`lang-${language}`).classList.add("selected");
+  localStorage.setItem("preferredLanguage", language);
 }
 
 function loadPreferredLanguage() {
@@ -116,6 +144,12 @@ function loadPreferredLanguage() {
   if (preferredLanguage) {
     changeLanguage(preferredLanguage);
   }
+}
+
+function toggleDropdown(event) {
+  event.preventDefault();
+  const dropdown = event.target.closest(".dropdown");
+  dropdown.classList.toggle("active");
 }
 
 document
@@ -127,5 +161,14 @@ document
 document
   .getElementById("lang-kg")
   .addEventListener("click", () => changeLanguage("kg"));
+
+document.addEventListener("click", function (event) {
+  const isClickInside = document
+    .querySelector(".dropdown")
+    .contains(event.target);
+  if (!isClickInside) {
+    document.querySelector(".dropdown").classList.remove("active");
+  }
+});
 
 window.onload = loadPreferredLanguage;
