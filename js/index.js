0fd65a479acb47b057bea93a91a0aafa0945f738
berem-git <berembek05@gmail.com>
2024-05-31 00:14:35 +0600

    t

--- js/index.js
+++ js/index.js
@@ -393,4 +393,21 @@ document.querySelectorAll(".dropdown-content a").forEach((item) => {
   item.addEventListener("click", hideMenu);
 });
 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    const offset = 100; // Отступ в пикселях
+    const elementPosition = targetElement.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
 window.onload = loadPreferredLanguage;
